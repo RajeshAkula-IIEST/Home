d4faea87a2fd04a8ab4736387f220e689166b087
RajeshAkula-IIEST <akula.mech@faculty.iiests.ac.in>
2026-07-26 11:02:03 +0530

    Create Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,52 @@
+export default function Footer(){
+
+return(
+
+<footer
+id="contact"
+className="bg-[#081829] text-white py-16"
+>
+
+<div className="container-custom">
+
+<h2 className="text-3xl font-bold">
+
+Dr. Rajesh Akula
+
+</h2>
+
+<p className="mt-5">
+
+Assistant Professor
+
+</p>
+
+<p>
+
+Department of Mechanical Engineering
+
+</p>
+
+<p>
+
+IIEST Shibpur
+
+</p>
+
+<div className="mt-10 border-t border-gray-700 pt-8">
+
+<p>
+
+© 2026 Dr. Rajesh Akula
+
+</p>
+
+</div>
+
+</div>
+
+</footer>
+
+)
+
+}
