bdd93149d2c690b122be3a524c7d91c2b5aa6e19
RajeshAkula-IIEST <akula.mech@faculty.iiests.ac.in>
2026-07-26 11:23:37 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,26 +4,24 @@ import About from "@/components/About";
 import Stats from "@/components/Stats";
 import Research from "@/components/Research";
 import Footer from "@/components/Footer";
+import ScrollProgress from "@/components/ScrollProgress";
+import ScrollTop from "@/components/ScrollTop";
 
 export default function HomePage() {
   return (
     <>
+      <ScrollProgress />
       <Navbar />
 
       <main>
-
         <Hero />
-
         <About />
-
         <Stats />
-
         <Research />
-
       </main>
 
       <Footer />
-
+      <ScrollTop />
     </>
   );
 }
