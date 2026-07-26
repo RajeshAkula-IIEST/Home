a75978c1da6e52121cbecb2edd2cf3e9ede75bf5
RajeshAkula-IIEST <akula.mech@faculty.iiests.ac.in>
2026-07-26 10:55:38 +0530

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+
+export default function HomePage() {
+  return (
+    <main className="min-h-screen">
+
+      <Navbar />
+
+      <Hero />
+
+    </main>
+  );
+}
